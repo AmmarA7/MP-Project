5568d7b5eb4316d8618646e6ac24ad605815cb55
Ammar <qvalrk.uni@gmail.com>
2026-06-13 18:34:36 +0300

    I2C testing

--- FinalProject.c
+++ FinalProject.c
@@ -13,43 +13,93 @@
 #define _XTAL_FREQ 4000000
 
 // --- PIN DEFINITIONS ---
-#define RS RC0
+#define RS RC0  
 #define E RC1   
 #define D4 RD4
 #define D5 RD5
 #define D6 RD6
 #define D7 RD7
 
-// HC-SR04 Pins (SWAPPED FOR HARDWARE INTERRUPT!)
-#define TRIG RB1 // Changed to RB1
-#define ECHO RB0 // MUST be RB0 for External Interrupt (INT)
+#define TRIG RB1 
+#define ECHO RB0 
 #define TRIG_DIR TRISBbits.TRISB1
 #define ECHO_DIR TRISBbits.TRISB0
 
-// Alarm Pins
 #define ALARM_LED RD0
 #define BUZZER RD1
 #define ALARM_LED_DIR TRISDbits.TRISD0
 #define BUZZER_DIR TRISDbits.TRISD1
 
 // --- GLOBAL VARIABLES ---
-// ISR Watchdog variables
 volatile unsigned int apnea_timer = 0;   
 volatile unsigned int ms_counter = 0;    
 volatile unsigned int seconds_passed = 0;
 volatile unsigned char alarm_active = 0; 
-
-// Ultrasonic Interrupt variables
 volatile unsigned int dist_cm = 0;
-volatile unsigned char echo_state = 0; // 0 = Waiting for Rising Edge, 1 = Waiting for Falling Edge
+volatile unsigned char echo_state = 0; 
 
-// --- INTERRUPT SERVICE ROUTINE (ISR) ---
-void __interrupt() ISR(void) {
+// --- I2C DRIVER FUNCTIONS (NEW) ---
+void i2c_init(void) {
+    TRISCbits.TRISC3 = 1; // SCL must be input
+    TRISCbits.TRISC4 = 1; // SDA must be input
+    SSPCON = 0x28;        // Enable SSP port, I2C Master mode
+    SSPCON2 = 0x00;
+    SSPSTAT = 0x00;
+    SSPADD = (_XTAL_FREQ / (4 * 100000)) - 1; // Set I2C clock to 100kHz
+}
+
+void i2c_wait(void) {
+    while ((SSPSTAT & 0x04) || (SSPCON2 & 0x1F)); // Wait until I2C bus is idle
+}
+
+void i2c_start(void) {
+    i2c_wait();
+    SEN = 1; // Initiate Start condition
+}
+
+void i2c_stop(void) {
+    i2c_wait();
+    PEN = 1; // Initiate Stop condition
+}
+
+void i2c_write(unsigned char data) {
+    i2c_wait();
+    SSPBUF = data; // Load data into buffer to transmit
+}
+
+unsigned char i2c_read(unsigned char ack) {
+    unsigned char temp;
+    i2c_wait();
+    RCEN = 1; // Enable receive mode
+    i2c_wait();
+    temp = SSPBUF; // Read data from buffer
+    i2c_wait();
+    ACKDT = (ack) ? 0 : 1; // Acknowledge bit (0 = ACK, 1 = NACK)
+    ACKEN = 1; // Send Acknowledge sequence
+    return temp;
+}
+
+// --- MAX30102 PING TEST FUNCTION (NEW) ---
+unsigned char ping_max30102(void) {
+    unsigned char part_id;
     
-    // 1. TIMER0 INTERRUPT: Apnea Watchdog (Ticks every 65.5ms)
+    i2c_start();
+    i2c_write(0xAE); // MAX30102 I2C Write Address (0xAE)
+    i2c_write(0xFF); // Point to Part ID Register (0xFF)
+    
+    i2c_start();     // Repeated start for reading
+    i2c_write(0xAF); // MAX30102 I2C Read Address (0xAF)
+    part_id = i2c_read(0); // Read 1 byte, send NACK to finish
+    i2c_stop();
+    
+    return part_id;
+}
+
+
+// --- INTERRUPT SERVICE ROUTINE ---
+void __interrupt() ISR(void) {
     if (TMR0IE && TMR0IF) {
         TMR0IF = 0; 
-        
         ms_counter++;
         apnea_timer++;
         
@@ -57,8 +107,7 @@ void __interrupt() ISR(void) {
             ms_counter = 0;
             seconds_passed++;
         }
-
-        if (apnea_timer > 90) { // ~6 seconds without a breath
+        if (apnea_timer > 90) { 
             alarm_active = 1;
             ALARM_LED = 1; 
             BUZZER = 1;    
@@ -69,33 +118,28 @@ void __interrupt() ISR(void) {
         }
     }
 
-    // 2. EXTERNAL INTERRUPT (RB0/INT): Ultrasonic Echo
     if (INTE && INTF) {
         if (echo_state == 0) {
-            // RISING EDGE: Sound wave just fired
             TMR1H = 0;
             TMR1L = 0;
-            TMR1ON = 1;              // Start counting microseconds
-            OPTION_REG &= ~(1 << 6); // Flip INTEDG bit to 0 (Look for Falling Edge next)
+            TMR1ON = 1;              
+            OPTION_REG &= ~(1 << 6); 
             echo_state = 1;
         } else {
-            // FALLING EDGE: Sound wave returned
-            TMR1ON = 0;              // Stop counting
+            TMR1ON = 0;              
             unsigned int time_taken = (TMR1H << 8) | TMR1L;
-            dist_cm = time_taken / 58; // Calculate distance
-            
-            OPTION_REG |= (1 << 6);  // Flip INTEDG bit back to 1 (Look for Rising Edge)
+            dist_cm = time_taken / 58; 
+            OPTION_REG |= (1 << 6);  
             echo_state = 0;
         }
-        INTF = 0; // Clear the interrupt flag
+        INTF = 0; 
     }
     
-    // 3. TIMER1 OVERFLOW: Sensor Timeout (In case the sound wave is lost forever)
     if (TMR1IE && TMR1IF) {
         TMR1ON = 0;
         echo_state = 0;
-        OPTION_REG |= (1 << 6); // Reset to looking for Rising Edge
-        TMR1IF = 0; // Clear flag
+        OPTION_REG |= (1 << 6); 
+        TMR1IF = 0; 
     }
 }
 
@@ -106,7 +150,6 @@ void lcd_enable_pulse(void) {
     E = 0;
     __delay_us(100);
 }
-
 void lcd_send_nibble(unsigned char nibble) {
     D4 = (nibble >> 0) & 0x01;
     D5 = (nibble >> 1) & 0x01;
@@ -114,21 +157,18 @@ void lcd_send_nibble(unsigned char nibble) {
     D7 = (nibble >> 3) & 0x01;
     lcd_enable_pulse();          
 }
-
 void lcd_command_4bit(unsigned char cmd) {
     RS = 0;                      
     lcd_send_nibble(cmd >> 4);  
     lcd_send_nibble(cmd & 0x0F);
     __delay_ms(2);              
 }
-
 void lcd_data_4bit(unsigned char data) {
     RS = 1;                      
     lcd_send_nibble(data >> 4);  
     lcd_send_nibble(data & 0x0F);
     __delay_ms(2);
 }
-
 void lcd_init_4bit(void) {
     TRISCbits.TRISC0 = 0;
     TRISCbits.TRISC1 = 0;
@@ -152,14 +192,12 @@ void lcd_init_4bit(void) {
     __delay_ms(2);
     lcd_command_4bit(0x06);
 }
-
 void lcd_set_cursor_4bit(unsigned char row, unsigned char col) {
     unsigned char address;
     if (row == 1) address = 0x80 + col;
     else address = 0xC0 + col;
     lcd_command_4bit(address);
 }
-
 void lcd_string_4bit(const char *str) {
     while (*str != '\0') {
         lcd_data_4bit(*str);    
@@ -172,7 +210,6 @@ void adc_init(void) {
     ADCON0 = 0x41; 
     ADCON1 = 0x8E; 
 }
-
 unsigned int adc_read(void) {
     __delay_ms(2);        
     GO_nDONE = 1;         
@@ -183,18 +220,11 @@ unsigned int adc_read(void) {
 // --- MAIN PROGRAM ---
 void main(void) {
     char display_buffer[16];
-    unsigned int adc_val;
-    float voltage;
-    int temp_c; 
-    
-    // Respiratory tracking variables
-    unsigned int breaths_in_window = 0;
-    unsigned int resp_rate_bpm = 0;
-    unsigned char inhaling = 0;
     
     // Initializations
     lcd_init_4bit();
     adc_init();
+    i2c_init(); // Initialize the I2C bus!
     
     // Pin Directions
     TRISAbits.TRISA0 = 1; 
@@ -202,46 +232,60 @@ void main(void) {
     ECHO_DIR = 1;         
     ALARM_LED_DIR = 0; 
     BUZZER_DIR = 0;    
-    
     ALARM_LED = 0; 
     BUZZER = 0;
     
+    // Check MAX30102 Connection Before Starting
+    unsigned char max_id = ping_max30102();
+    sprintf(display_buffer, "MAX ID: 0x%02X", max_id);
+    lcd_set_cursor_4bit(1, 0);
+    lcd_string_4bit(display_buffer);
+    
+    if (max_id == 0x15) {
+        lcd_set_cursor_4bit(2, 0);
+        lcd_string_4bit("I2C SUCCESS!    ");
+    } else {
+        lcd_set_cursor_4bit(2, 0);
+        lcd_string_4bit("I2C FAILED!     ");
+    }
+    
+    // Pause for 3 seconds so you can read the result before the Apnea code takes over
+    __delay_ms(1000);
+    __delay_ms(1000);
+    __delay_ms(1000);
+    lcd_command_4bit(0x01); // Clear screen
+    
     // --- CONFIGURE INTERRUPTS ---
-    // Timer0 (Apnea Watchdog)
     OPTION_REG &= 0xC0; 
-    OPTION_REG |= 0x07; // 1:256 prescaler
+    OPTION_REG |= 0x07; 
     TMR0IE = 1; 
-    
-    // External Interrupt RB0 (Ultrasonic Echo)
-    OPTION_REG |= (1 << 6); // INTEDG = 1 (Interrupt on Rising Edge to start)
+    OPTION_REG |= (1 << 6); 
     INTE = 1; 
-    
-    // Timer1 (Ultrasonic Counter)
     T1CON = 0x00; 
-    TMR1IE = 1; // Enable overflow interrupt just in case
-    
-    // Enable Global and Peripheral Interrupts
+    TMR1IE = 1; 
     PEIE = 1;   
     GIE = 1;    
+    
+    unsigned int breaths_in_window = 0;
+    unsigned int resp_rate_bpm = 0;
+    unsigned char inhaling = 0;
+    unsigned int adc_val;
+    float voltage;
+    int temp_c; 
         
     while(1) {
-        
-        // 1. Send the Trigger Pulse (Non-blocking!)
-        if (echo_state == 0) { // Only fire if we aren't currently waiting for an echo
+        if (echo_state == 0) { 
             TRIG = 1;
             __delay_us(10);
             TRIG = 0;
         }
         
-        // 2. Breath Detection Logic (Using 'dist_cm' which is magically updated by the ISR)
         if (dist_cm > 0 && dist_cm < 50 && inhaling == 0) {
             inhaling = 1;
             breaths_in_window++;
-            
             GIE = 0; 
             apnea_timer = 0; 
             GIE = 1; 
-            
         } else if (dist_cm >= 50 && inhaling == 1) {
             inhaling = 0;
             GIE = 0; 
@@ -249,22 +293,18 @@ void main(void) {
             GIE = 1; 
         }
         
-        // 3. Calculate BPM every 15 Seconds
         if (seconds_passed >= 15) {
             resp_rate_bpm = breaths_in_window * 4; 
-            
             GIE = 0;
             breaths_in_window = 0;
             seconds_passed = 0;
             GIE = 1;
         }
         
-        // 4. Read Temperature
         adc_val = adc_read();
         voltage = (adc_val * 5.0) / 1023.0;
         temp_c = (int)(voltage * 100.0);
         
-        // 5. Update LCD Screen
         if (alarm_active) {
             sprintf(display_buffer, "WARNING: APNEA! ");
         } else {
@@ -277,7 +317,6 @@ void main(void) {
         lcd_set_cursor_4bit(2, 0);
         lcd_string_4bit(display_buffer);
         
-        // A short 100ms delay to prevent the screen from flickering and give the sensor time to rest
         __delay_ms(100); 
     }
 }
